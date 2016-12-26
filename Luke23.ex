d5ac4771bee7f3bebae5c4a44a6dcad73cf1f43b
Morten Teinum <morten.teinum@gmail.com>
2016-12-26 12:50:11 +0100

    Update Luke23.ex

--- Luke23.ex
+++ Luke23.ex
@@ -1,4 +1,5 @@
-# Et barn løper opp en trapp med 250 trinn, og kan ta enten ett, to eller tre steg om gangen. Hvor mange ulike måter kan barnet løpe opp trappen?
+# Et barn løper opp en trapp med 250 trinn, og kan ta enten ett, to eller tre steg om gangen. Hvor mange ulike måter
+# kan barnet løpe opp trappen?
 defmodule Luke23 do
 	def stairs(_, _, c, 0), do: c
 	def stairs(a, b, c, n), do: stairs(b, c, a + b + c, n - 1) 
