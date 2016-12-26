f97d5d1b2ceb86d2491ab17826b0818c8b89138c
Morten Teinum <morten.teinum@gmail.com>
2016-12-26 12:46:31 +0100

    Update Luke15.ex

--- Luke15.ex
+++ Luke15.ex
@@ -1,7 +1,11 @@
 # Look-and-say
-# Du er en gjeng på hytta som er lei av å spille alias og vri-åtter. En i gjengen foreslår at vi skal spille noe som heter Look-and-Say (her).
-# Spillet går ut på at en person velger en vilkårlig sekvens, som for eksempel 1131. En annen i gjengen skal lese tallet og skrive den nye sekvensen ut ifra måten det er sagt på høyt.
-# For å demonstrere eksempelet over, så leser du det første tallet høyt => to enere, en treer, en ener, som da blir => 211311. Grunnen til dette er at slike sekvenser er generert iterativt. Hvis det kommer flere tall etter hverandre, som 11 i tallet over, så teller man hvor mange av samme tall det er (2) og legger ved selve tallet (1) etterpå.
+# Du er en gjeng på hytta som er lei av å spille alias og vri-åtter. En i gjengen foreslår at vi skal spille noe som heter
+# Look-and-Say (her).
+# Spillet går ut på at en person velger en vilkårlig sekvens, som for eksempel 1131. En annen i gjengen skal lese tallet og
+# skrive den nye sekvensen ut ifra måten det er sagt på høyt.
+# For å demonstrere eksempelet over, så leser du det første tallet høyt => to enere, en treer, en ener, som da blir => 211311.
+# Grunnen til dette er at slike sekvenser er generert iterativt. Hvis det kommer flere tall etter hverandre, som 11 i tallet
+# over, så teller man hvor mange av samme tall det er (2) og legger ved selve tallet (1) etterpå.
 # Eksempel:
 # 3 => 13 (en 3er)
 # 13 => 1113 (en 1er, en 3er)
