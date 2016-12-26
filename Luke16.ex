bb58609949d6e4fa565c15fc5ab65189e1e92a01
Morten Teinum <morten.teinum@gmail.com>
2016-12-26 12:46:49 +0100

    Update Luke16.ex

--- Luke16.ex
+++ Luke16.ex
@@ -1,4 +1,5 @@
-# Julenissen er glad i å leke med tall og å kombinere det med en gjettelek er alltid en slager. I dagens luke tenker han på et sekssifret tall hvor summen av sifrene er 43. Og hvor bare to av de tre påstandene under er sanne:
+# Julenissen er glad i å leke med tall og å kombinere det med en gjettelek er alltid en slager. I dagens luke tenker
+# han på et sekssifret tall hvor summen av sifrene er 43. Og hvor bare to av de tre påstandene under er sanne:
 # det er et kvadratisk tall
 # det er et kubisk tall
 # tallet er mindre enn 500 000
